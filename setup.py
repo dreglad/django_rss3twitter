e141626a26eab72173268e1ca7dd0d64c53b9be3
David Regla <dreglad@gmail.com>
2011-01-25 03:48:33 -0600

    agregado setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "rss3twitter",
+    version = "0.1",
+    url = '',
+    description = "rss3twitter",
+    packages = find_packages('.'),
+    package_dir = {'': '.'},
+    install_requires = ['setuptools'],
+)
